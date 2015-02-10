49b53ddfb7f1a219f9071749c88c9729db1759c0
James Rico <jamesrico089@gmail.com>
2015-02-10 11:22:40 -0600

    fixed form to submit to firebase

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,77 @@
 /* jshint jquery: true */
 /* global async: false */
 
-/* jshint jquery: true */
-/* global async: false */
-
 'use strict';
 
 function hello() {
   return 'world';
 }
 
+  var FIREBASE_URL = 'https://myaddressbookapp.firebaseio.com/.json';
+
+  $.get(FIREBASE_URL, function (contact) {
+    Object.keys(contact).forEach(function (uuid) {
+    addContactToTable(uuid, contact[uuid]);
+    });
+  });
+
+
+  $('#submitcontact').on('click', newContactSubmitForm);
+
+
+function newContactSubmitForm (evt) {
+
+  var $trs = $('tr'),
+      $FirstName = $('#firstname').val(),
+      $LastName = $('#lastname').val(),
+      $Email = $('#email').val(),
+      $Twitter = $('#twitter').val(),
+      $PhotoURL = $('#photoURL').val();
+
+  var res = {FirstName: $FirstName, LastName: $LastName, Email: $Email, Twitter: $Twitter, PhotoURL: $PhotoURL};
+
+  var url = FIREBASE_URL,
+      jsonifiedData = JSON.stringify(res);
+
+  console.log(url);
+  console.log(jsonifiedData);
+
+  $.post(url, jsonifiedData, function(res){ return res;});
+
+  $('#newcontactform').submit( function (evt){
+      evt.preventDefault();
+      alert('Contact Submitted!');
+      document.getElementById('newcontactform').reset();
+  });
+
+ };
+
+function addContactToTable(uuid, res) {
+};
+
+$('tbody').on('click', '#removebutton', function (evt) {
+
+  var $tr  = $(evt.target).closest('tr'),
+      uuid = $tr.data('uuid'),
+      friendName = $tr.find('tr').text();
+
+  if (confirmContactRemoval(friendName)) {
+    $tr.remove();
+    deleteContactFromFirebase(uuid);
+  }
+});
+
+function deleteContactFromFirebase(uuid) {
+  var url = FIREBASE_URL + uuid + '.json';
+
+  $.ajax(url, {type: 'DELETE'});
+}
+
+function confirmContactRemoval(contactName) {
+  var confirmationText = 'Remove ' + friendName + ' from friend list?',
+      isConfirmed      = window.confirm(confirmationText);
+
+  return isConfirmed;
+}
 
 
--- test/spec/main.test.js
+++ test/spec/main.test.js
@@ -8,8 +8,25 @@ describe('test suite', function () {
   });
 });
 
-describe('hello', function () {
-  it('should return world', function () {
-    hello().should.equal('world');
+describe('DOM', function () {
+
+describe('firebase', function (newcontact) {
+  it('should submit the form to firebase', function () {
+    var newcontacts = [
+                        {FirstName: 'Kobe', LastName: 'Bryant', Email: 'blackmamba24@gmail.com', Twitter: 'blackmamba24', PhotoURL: 'nba.com/lakers/kobe'},
+                        {FirstName: 'Michael', LastName: 'Jordan', Email: 'airjordan23@gmail.com', Twitter: 'airjordan23', PhotoURL: 'nba.com/mj'}        
+                      ],
+        $tbody = $('tbody'),
+        $trs;
+
+    $tbody.append('<tr><td>Kobe</td><td>Bryant</td><td>blackmamba24@gmail.com</td><td>blackmamba24</td><td>nba.com/kobe</td></tr>');
+    $tbody.append('<tr><td>Michael</td><td>Jordan</td><td>airjordan23@gmail.com</td><td>airjordan23</td><td>nba.com/mj</td></tr>');
+
+    $trs = $('tr');
+
+    newContactSubmitForm(newcontacts);
+
+    $($($trs[0]).find('td')[0]).text().should.equal('Kobe');
+    });
   });
 });
